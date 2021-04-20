01babeb0d05b20e43c1c7bcd5e6a2b946bb3196d
Murilo-hfs <78485648+Murilo-hfs@users.noreply.github.com>
2021-04-20 17:29:05 -0300

    continuando os estudos no app shop

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -45,6 +45,21 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,  
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(
+                  'Produto adicionado com sucesso!',
+                  textAlign: TextAlign.center,
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'DESFAZER', 
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  }
+                  ),
+                ),
+                );
               cart.addItem(product);
             },
           ),
